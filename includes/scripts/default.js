24d926ae3e27874ec6b47eb01f4f4a105dba6c48
Simon Arthur <github@arthur12.co.uk>
2020-06-18 15:07:06 +0100

    Update to CSS
    * new LineAwesome version implemented.
    * new shrinking header added.
    * Back to Top arrow added.

--- includes/scripts/default.js
+++ includes/scripts/default.js
@@ -1,5 +1,45 @@
+/* --- DEFAULT SCRIPTS ----- */
+
 
 
 $('[data-fancybox]').fancybox({
 	protect: true
 })
+
+
+
+/* --- change icon on menu ----- ----- */
+$("input#menu").change(function() {
+	if(this.checked) {
+		$("input#menu .las").removeClass('la-bars').addClass('la-caret-down');
+	}
+});
+
+
+
+$('input#menu').mousedown(function() {
+    if (!$(this).is(':checked')) {
+		$("input#menu .las").removeClass('la-bars').addClass('la-caret-down');
+    }
+});
+
+
+
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+window.onscroll = function() {
+	scrollFunction()
+};
+
+function scrollFunction() {
+	
+	if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+		$('#header').addClass("narrow").removeClass("broad");
+		$('#header h1').addClass("hidden").removeClass("inline");
+		$('#button-top').addClass("inline").removeClass("hidden");
+	} else {
+		$('#header').addClass("broad").removeClass("narrow");
+		$('#header h1').addClass("inline").removeClass("hidden");
+		$('#button-top').addClass("hidden").removeClass("inline");
+	}
+	
+}
