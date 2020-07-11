81a92086ae0ae933576c2361a476ba134f4b7ec1
anomis66 <github@simonarthur.co.uk>
2020-07-11 02:03:05 +0100

    New Dark Theme

--- includes/scripts/default.js
+++ includes/scripts/default.js
@@ -1,4 +1,6 @@
-/* --- DEFAULT SCRIPTS ----- */
+/*
+# JavaScript and jQuery 
+*/
 
 
 
@@ -8,24 +10,8 @@ $('[data-fancybox]').fancybox({
 
 
 
-/* -- Change Icon On Menu Drop-Down----- */
-$(document).ready(function() { 
-	$("#nav #menu").change(function() {
-		if($(this).is(':checked')) {
-			$("#nav label i").removeClass('la-caret-square-down').addClass('la-times-circle');
-		} 
-		if(!$(this).is(':checked')) {
-			$("#nav label i").removeClass('la-times-circle').addClass('la-caret-square-down');
-		}
-		
-	});
-});
-
-
-
 /* -- On-Scroll Update ----- *
- * When the user scrolls down 100px from the 
- * top of the document, trigger a reaction. 
+ * When the user scrolls down 100px from the top of the document, trigger a reaction. 
  */
 window.onscroll = function() {
 	scrollFunction()
