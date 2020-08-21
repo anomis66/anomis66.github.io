c9d550ad2b1a5befd1b586214bf6bbb7e7da416e
anomis66 <github@simonarthur.co.uk>
2020-08-22 00:37:46 +0100

    Caption Slider
    Added a :hover slide to figure captions

--- includes/scripts/default.js
+++ includes/scripts/default.js
@@ -18,7 +18,6 @@ window.onscroll = function() {
 };
 
 function scrollFunction() {
-	
 	if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
 		$('#header').addClass("narrow").removeClass("broad");
 		$('#button-top').addClass("inline").removeClass("hidden");
@@ -26,5 +25,52 @@ function scrollFunction() {
 		$('#header').addClass("broad").removeClass("narrow");
 		$('#button-top').addClass("hidden").removeClass("inline");
 	}
-	
 }
+
+$(document).ready(function() {
+/*
+	$('.standard').hover(
+		function(){
+			$(this).find('.caption').show();
+		},
+		function(){
+			$(this).find('.caption').hide();
+		}
+	);
+	$('.fade').hover(
+		function(){
+			$(this).find('.caption').fadeIn(250);
+		},
+		function(){
+			$(this).find('.caption').fadeOut(250);
+		}
+	);
+*/
+	$('figure').hover(
+		function(){
+			$(this).find('figcaption').slideDown(250);
+		},
+		function(){
+			$(this).find('figcaption').slideUp(250);
+		}
+	);
+});
+
+/*
+$('figure img').hover(function(){
+	var $caption = $("figcaption");
+	if ($caption.is('hidden')){
+		$caption.slideDown('slow');
+	} else {
+		$caption.slideUp('slow');
+	}
+});
+
+/*
+$('figure a img').hover(function() {
+	if ($("figcaption").is('hidden')) {
+		$(this).show('slideUp', 'slow');
+		$(this).hide('slideDown', 'slow');
+	};
+});
+*/
