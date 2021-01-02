c354476a203f94fdfe94e266593143d4106e4631
Simon Arthur <github@simonarthur.co.uk>
2021-01-02 16:50:52 +0000

    Core components
    New (old) logo
    local jQuery and other resources.

---
+++ includes/scripts/core.js
@@ -0,0 +1,49 @@
+/* ---- CORE FUNCTIONS --- */
+
+$( document ).ready(function() {
+    console.log( "Document Ready!" );
+
+
+
+/* DISPLAY IMAGE TITLE AS CAPTION
+–––––––––––––––––––––––––––––––––––––––––––––––––– */
+$("p img").each(
+	function(){
+		var title = $(this).attr('title');
+		var text = '<span class="caption">' + title + '</span>';
+		$(this).parent().append(text);
+	}
+);
+
+
+
+
+/* LOAD JSON NAVBAR
+–––––––––––––––––––––––––––––––––––––––––––––––––– */
+fetch('nav.json')
+.then(function (response) {
+	return response.json();
+})
+.then(function (data) {
+	appendData(data);
+})
+.catch(function (err) {
+	console.log(err);
+});
+
+
+function appendData(data) {
+	console.log('Records: ' + data.length);
+	for (var i = 0; i < data.length; i++) {
+		var myList = document.createElement("li");
+		myList.innerHTML = '<a href="' + data[i].url + '">' + data[i].title + '</a> <small>('+data[i].date+')</small>';
+		console.log(myList);
+		document.getElementById("navbar").appendChild(myList);
+	}
+}
+
+
+
+
+
+});
--- includes/scripts/default.js
+++ includes/scripts/default.js
@@ -1,76 +1,17 @@
-/*
-# JavaScript and jQuery 
-*/
+function include(file) { 
+  
+  var script  = document.createElement('script'); 
+  script.src  = file; 
+  script.type = 'text/javascript'; 
+  script.defer = true; 
+  
+  document.getElementsByTagName('head').item(0).appendChild(script); 
+  
+} 
+  
+/* Include Many js files */
+include('https://www.simonarthur.co.uk/includes/scripts/jquery-3.5.1.min.js'); 
+include('https://www.simonarthur.co.uk/includes/scripts/jquery.fancybox.min.js');
+include('https://www.simonarthur.co.uk/includes/scripts/core.js');
 
 
-
-$('[data-fancybox]').fancybox({
-	protect: true
-})
-
-
-
-/* -- On-Scroll Update ----- *
- * When the user scrolls down 100px from the top of the document, trigger a reaction. 
- */
-window.onscroll = function() {
-	scrollFunction()
-};
-
-function scrollFunction() {
-	if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-		$('#header').addClass("narrow").removeClass("broad");
-		$('#button-top').addClass("inline").removeClass("hidden");
-	} else {
-		$('#header').addClass("broad").removeClass("narrow");
-		$('#button-top').addClass("hidden").removeClass("inline");
-	}
-}
-
-$(document).ready(function() {
-/*
-	$('.standard').hover(
-		function(){
-			$(this).find('.caption').show();
-		},
-		function(){
-			$(this).find('.caption').hide();
-		}
-	);
-	$('.fade').hover(
-		function(){
-			$(this).find('.caption').fadeIn(250);
-		},
-		function(){
-			$(this).find('.caption').fadeOut(250);
-		}
-	);
-*/
-	$('figure').hover(
-		function(){
-			$(this).find('figcaption').slideDown(250);
-		},
-		function(){
-			$(this).find('figcaption').slideUp(250);
-		}
-	);
-});
-
-/*
-$('figure img').hover(function(){
-	var $caption = $("figcaption");
-	if ($caption.is('hidden')){
-		$caption.slideDown('slow');
-	} else {
-		$caption.slideUp('slow');
-	}
-});
-
-/*
-$('figure a img').hover(function() {
-	if ($("figcaption").is('hidden')) {
-		$(this).show('slideUp', 'slow');
-		$(this).hide('slideDown', 'slow');
-	};
-});
-*/
