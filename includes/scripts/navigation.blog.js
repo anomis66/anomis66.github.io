024933ca45e8bddc4260a3b62f31bf9d7cb2d479
Simon Arthur <github@simonarthur.co.uk>
2021-01-04 23:18:16 +0000

    Separating Navigation

---
+++ includes/scripts/navigation.blog.js
@@ -0,0 +1,24 @@
+/* LOAD MAIN NAVIGATION
+------------------------------ */
+
+fetch('https://www.simonarthur.co.uk/items/blog.json')
+.then(function (response) {
+	return response.json();
+})
+.then(function (data) {
+	appendData(data);
+})
+.catch(function (err) {
+	console.log(err);
+});
+
+
+function appendData(data) {
+	console.log('Records: ' + data.length);
+	for (var i = 0; i < data.length; i++) {
+		var myList = document.createElement("li");
+		myList.innerHTML = '<a href="' + data[i].url + '">' + data[i].title + '</a> <small>('+data[i].date+')</small>';
+		// console.log(myList);
+		document.getElementById("myNavbar").appendChild(myList);
+	}
+}
